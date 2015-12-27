05cf93ab24356f6e88bce4ccdc172e2ff8ab0b7d
Otto Fox <ofox@lifesize.com>
2015-12-26 18:44:51 -0600

    Added dropTable, replaced System.out with pay.Logger

--- app/services/DynamoDBService.java
+++ app/services/DynamoDBService.java
@@ -1,28 +1,21 @@
 package services;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.AmazonWebServiceRequest;
+import com.amazonaws.ResponseMetadata;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.Condition;
-import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
-import com.amazonaws.services.dynamodbv2.model.DescribeTableRequest;
-import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
-import com.amazonaws.services.dynamodbv2.model.KeyType;
-import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
-import com.amazonaws.services.dynamodbv2.model.PutItemResult;
-import com.amazonaws.services.dynamodbv2.model.QueryRequest;
-import com.amazonaws.services.dynamodbv2.model.QueryResult;
-import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
-import com.amazonaws.services.dynamodbv2.model.ScanRequest;
-import com.amazonaws.services.dynamodbv2.model.ScanResult;
-import com.amazonaws.services.dynamodbv2.model.TableDescription;
-import com.amazonaws.services.dynamodbv2.model.TableStatus;
+
+import com.amazonaws.services.dynamodbv2.model.*;
+
+import play.Logger;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DynamoDBService {
 
@@ -40,11 +33,11 @@ public class DynamoDBService {
             .withAttributeDefinitions(new AttributeDefinition().withAttributeName(primaryKeyAttributeName).withAttributeType(ScalarAttributeType.S))
             .withProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(1L).withWriteCapacityUnits(1L));
         TableDescription createdTableDescription = dynamoDB.createTable(createTableRequest).getTableDescription();
-        System.out.println("Created Table: " + createdTableDescription);
+        Logger.info("Created Table: " + createdTableDescription);
     }
 
     public static void waitForTableToBecomeAvailable(String tableName) {
-        System.out.println("Waiting for " + tableName + " to become ACTIVE...");
+        Logger.info("Waiting for " + tableName + " to become ACTIVE...");
 
         long startTime = System.currentTimeMillis();
         long endTime = startTime + (10 * 60 * 1000);
@@ -53,7 +46,7 @@ public class DynamoDBService {
                 DescribeTableRequest request = new DescribeTableRequest().withTableName(tableName);
                 TableDescription tableDescription = dynamoDB.describeTable(request).getTable();
                 String tableStatus = tableDescription.getTableStatus();
-                System.out.println("  - current state: " + tableStatus);
+                Logger.info("  - current state: " + tableStatus);
                 if (tableStatus.equals(TableStatus.ACTIVE.toString())) return;
             } catch (AmazonServiceException ase) {
                 if (ase.getErrorCode().equalsIgnoreCase("ResourceNotFoundException") == false) throw ase;
@@ -71,34 +64,37 @@ public class DynamoDBService {
     public static void describeTable(String tableName) {
         DescribeTableRequest describeTableRequest = new DescribeTableRequest().withTableName(tableName);
         TableDescription tableDescription = dynamoDB.describeTable(describeTableRequest).getTable();
-        System.out.println("Table Description: " + tableDescription);
+        Logger.info("Table Description: " + tableDescription);
     }
 
     public static void putItem(String tableName, Map<String, AttributeValue> item) {
         PutItemRequest putItemRequest = new PutItemRequest(tableName, item);
         PutItemResult putItemResult = dynamoDB.putItem(putItemRequest);
-        System.out.println("Result: " + putItemResult);
+        Logger.info("Result: " + putItemResult);
     }
 
     public static ScanResult scan(String tableName, HashMap<String, Condition> scanFilter) {
         ScanRequest scanRequest = new ScanRequest(tableName).withScanFilter(scanFilter);
         ScanResult scanResult = dynamoDB.scan(scanRequest);
-        System.out.println("Result: " + scanResult);
+        Logger.info("Result: " + scanResult);
         return scanResult;
     }
 
     public static ScanResult scan(String tableName) {
         ScanRequest scanRequest = new ScanRequest(tableName);
         ScanResult scanResult = dynamoDB.scan(scanRequest);
-        System.out.println("Result: " + scanResult);
+        Logger.info("Result: " + scanResult);
         return scanResult;
     }
 
     public static QueryResult query(String tableName) {
         QueryRequest queryRequest = new QueryRequest(tableName);
         QueryResult queryResult = dynamoDB.query(queryRequest);
-        System.out.println("Result: " + queryResult);
+        Logger.info("Result: " + queryResult);
         return queryResult;
     }
 
+    public static void dropTable(String tableName) {
+       dynamoDB.deleteTable(tableName);
+    }
 }
